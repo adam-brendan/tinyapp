d70c3c58925c59b581df81c8f02297b890155499
adam-brendan <adamcraveiro@gmail.com>
2019-05-07 16:52:31 +0000

    Added a route

--- express_server.js
+++ express_server.js
@@ -1,5 +1,5 @@
 var express = require("express");
-var app = express();
+var app = express(); //What is this function actually doing?
 var PORT = 8080; // Default port 8080
 
 var urlDatabase = {
@@ -7,8 +7,12 @@ var urlDatabase = {
   "9sm5xK": "http://www.google.com"
 };
 
-app.get("/", (req, res) => { //Where are these parameters coming from? Registers a handler on the root path?
-  res.send("Hello!") // What is res.send? Res is an object?
+// app.get("/", (req, res) => { //Where are these parameters coming from? Registers a handler on the root path?
+//   res.send("Hello!") // What is res.send? Res is an object?
+// });
+
+app.get("/urls.json", (req, res) => {
+  res.json(urlDatabase);
 });
 
 app.listen(PORT, () => { //Why does this function not take any parameters?
